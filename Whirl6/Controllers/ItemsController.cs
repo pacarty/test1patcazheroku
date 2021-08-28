9f3a845b60c247fc45050546e0da75746bc48d0c
Patrick Carty <carty.patrick@gmail.com>
2021-08-28 10:55:51 +1000

    First commit

---
+++ Whirl6/Controllers/ItemsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Whirl6.Models;
+
+namespace Whirl6.Controllers
+{
+    [Route("api/[controller]")]
+    public class ItemsController : Controller
+    {
+        private TodoContext _context;
+
+        public ItemsController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("GetItems")]
+        public IActionResult GetItems()
+        {
+            var result = _context.TodoItems.ToList();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("PostItem")]
+        public IActionResult PostItem(TodoItem item)
+        {
+            //TodoItem item = new TodoItem { Name = name, IsComplete = isComplete };
+
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ Whirl6/wwwroot/js/site.js
@@ -0,0 +1,31 @@
+﻿function loadPage() {
+    document.getElementById("thing").innerHTML = "ballso";
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("thing").innerHTML = this.responseText;
+        }
+    };
+
+    xhttp.open("GET", "api/Items/GetItems");
+    xhttp.send();
+}
+
+function postThing() {
+    var answer = document.getElementById("sometext").value;
+
+    var data = new FormData();
+    data.append("name", answer);
+    data.append("isComplete", true);
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    };
+
+    xhttp.open("POST", "api/Items/PostItem");
+    xhttp.send(data);
+}
